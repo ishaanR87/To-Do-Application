18ded8967d70c3525ad7ac7a2d11d17b33495ff1
Ishaan Rawat <i.rawat98@gmail.com>
2023-08-10 15:49:45 +0100

    adding heading + create task button

---
+++ myreactapp/src/components/ToDoList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ToDoList() {
+  return (
+    <>
+    <div className='header text-center'>
+        <h2>To-Do List</h2>
+        <button className='btn btn-primary mt-3'>Create task</button>
+    </div>
+</>
+  )
+}
