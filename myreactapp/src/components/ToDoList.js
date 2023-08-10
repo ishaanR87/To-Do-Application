78f5d57373c41c9546c8525f4083fb20857484d3
Ishaan Rawat <i.rawat98@gmail.com>
2023-08-10 16:47:05 +0100

    added pop-up (modal) for clicking create task button

--- myreactapp/src/components/ToDoList.js
+++ myreactapp/src/components/ToDoList.js
@@ -1,13 +1,25 @@
-import React from 'react'
-import createTask from '../modals/createTask'
+import React, {useState} from 'react'
+import CreateTask from '../modals/createTask'
 
 export default function ToDoList() {
+    const [modal, setModal] = useState(false);
+
+    const toggle = () => {
+      setModal(!modal); 
+    }
+
   return (
     <>
     <div className='header text-center'>
         <h2>To-Do List</h2>
-        <button className='btn btn-primary mt-3'>Create task</button>
+        <button className='btn btn-primary mt-3' onClick={() => setModal(true)}>Create task</button>
     </div>
-</>
+
+    <div className='task-container'>
+
+
+    </div>
+    <CreateTask toggle = {toggle} modal = {modal}/>
+    </>
   )
 }
