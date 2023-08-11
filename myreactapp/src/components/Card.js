2463e8edb67e7ce65913e0a06fb94a318ab06097
Ishaan Rawat <i.rawat98@gmail.com>
2023-08-11 09:54:21 +0100

    added card features to display task and descripton

--- myreactapp/src/components/Card.js
+++ myreactapp/src/components/Card.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
-export default function Card() {
+export default function Card({taskObj, index}) {
   return (
-    <div>Card</div>
-  )
-}
+        <div class = "card-wrapper mr-5">
+            <div class = "card-top" style={{"background-color": "#5DC250"}}></div>
+            <div class = "task-holder">
+                <span class = "card-header" style={{"background-color": "#F2FAF1", "border-radius": "10px"}}>
+                    {taskObj.Name}
+                </span>
+                <p style={{marginTop: 10}}>{taskObj.Description}</p>
+
+                <div style={{"position": "absolute", "right" : "20px", "bottom" : "20px"}}>
+                    <i class = "far fa-edit mr-3" style= {{ "color": "#5DC250"}} ></i>
+                    <i class="fas fa-trash-alt" style= {{"color": "#5DC250"}} ></i>
+                </div>
+        </div>
+        </div>
+    );
+};
+  
+
--- myreactapp/src/components/ToDoList.js
+++ myreactapp/src/components/ToDoList.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from 'react'
 import CreateTask from '../modals/CreateTask';
+import Card from './Card';
 
 export default function ToDoList() {
     const [modal, setModal] = useState(false);
@@ -33,7 +34,7 @@ export default function ToDoList() {
     </div>
 
     <div className='task-container'>
-        {taskList.map((obj) => <li>{obj.Name}</li>)}
+        {taskList && taskList.map((obj, index) => <Card taskObj = {obj} index = {index}/>)}
 
     </div>
     <CreateTask toggle = {toggle} modal = {modal} save = {saveTask}/>
