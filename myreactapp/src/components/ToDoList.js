cf45647f5c2fee703722bafe3ddd83d313d1006a
Ishaan Rawat <i.rawat98@gmail.com>
2023-08-11 13:32:52 +0100

    added images for readme

--- myreactapp/src/components/ToDoList.js
+++ myreactapp/src/components/ToDoList.js
@@ -48,7 +48,7 @@ const TodoList = () => {
     return (
         <>
             <div className = "header text-center">
-                <h3>Todo List</h3>
+                <h3>To-Do App</h3>
                 <button className = "btn btn-primary mt-2" onClick = {() => setModal(true)} >Create Task</button>
             </div>
             <div className = "task-container">
